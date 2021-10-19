ab433f8f0db1c813dfb1e8ba7df28a56c81be78c
guillermomari <guillermo.a.mari@gmail.com>
2021-10-19 11:03:07 -0300

    New Test un JEST , just for example

--- services/getAPIWeather.test.js
+++ services/getAPIWeather.test.js
@@ -2,10 +2,16 @@
 
 const {getApiWeather} = require('./getAPIWeather')
 
-describe('Verificacion de logica', ()=>{
-    test('ejecutar con alguna logica la API', ()=>{
-        let data =  getApiWeather('-33.1236', '-64.3492')
+describe('Logial conection verification', ()=>{
+    test('Exec API with result != Undefined',async ()=>{
+        let data = await getApiWeather('-33.1236', '-64.3492')
         expect(data).not.toBe(undefined)
+    })
+
+    test('Exec API with result  Undefined', async  ()=>{
+        let data =  await getApiWeather()
+        console.log(data)
+        expect(data).toEqual(undefined)
     })
 })
 
